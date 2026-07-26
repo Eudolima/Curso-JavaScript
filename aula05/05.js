2fd6df14057ece803134bd07e19f60ff4bc11912
Eudo <154047007+Eudolima@users.noreply.github.com>
2026-07-26 21:24:48 +0000

    Laços de Repetição While

---
+++ aula05/05.js
@@ -0,0 +1,6 @@
+// Exemplo 1: Repetição simples com WHILE
+var contador = 1;
+while (contador <= 3) {
+    alert("Executando a repetição número: " + contador);
+    contador++; // Atualização para evitar loop infinito
+}
